fa42355bdb32c990784e337bca10617fd9693ed1
alexandre-mace <helloalexandremace@protonmail.com>
2022-12-13 18:18:25 +0100

    Update share button

--- src/components/ShareLink.js
+++ src/components/ShareLink.js
@@ -2,27 +2,26 @@ import shareicon from "../assets/share.svg";
 import copyToClipboard from "../utils/copyToClipboard";
 
 const ShareLink = ({linkCopied, setLinkCopied}) => (
-    <div className="w-auto absolute top-3 right-8 md:top-6 md:right-6 z-[99]">
-        <div
-            onClick={() => {
-                setLinkCopied(true)
-                copyToClipboard('https://ledinerdusiecle.bonpote.com')
-                setTimeout(() => {
-                    setLinkCopied(false)
-                }, 2000)
-            }}
-            className="cursor-pointer md:mr-3 py-2 btn-quartary text-xs md:text-base">
-            {linkCopied &&
-                <div>✅ Lien copié</div>
-            }
-            {!linkCopied &&
-                <div className={'flex whitespace-nowrap'}>
-                    <img className="mr-2 h-4 md:h-6" src={shareicon} alt="pause"/>
-                    <div>Partager le lien</div>
-                </div>
-            }
-        </div>
-    </div>
+    <button
+        className="w-auto absolute top-3 right-8 md:top-6 md:right-6 z-[99] cursor-pointer md:mr-3 py-2 btn-quartary text-xs md:text-base"
+        onClick={() => {
+            setLinkCopied(true)
+            copyToClipboard('https://ledinerdusiecle.bonpote.com')
+            setTimeout(() => {
+                setLinkCopied(false)
+            }, 2000)
+        }}
+    >
+        {linkCopied &&
+            <div>✅ Lien copié</div>
+        }
+        {!linkCopied &&
+            <div className={'flex whitespace-nowrap'}>
+                <img className="mr-2 h-4 md:h-6" src={shareicon} alt="pause"/>
+                <div>Partager le lien</div>
+            </div>
+        }
+    </button>
 )
 
 export default ShareLink
