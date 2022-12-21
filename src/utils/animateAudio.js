547d78387d013b9607627bf214fe8423b2d57994
alexandre-mace <helloalexandremace@protonmail.com>
2022-12-21 03:19:32 +0100

    Fix potential animation reset when no node is found

--- src/utils/animateAudio.js
+++ src/utils/animateAudio.js
@@ -1,8 +1,10 @@
 const animateAudio = (duration) => {
-    document.getElementById('audioAnimation1').setAttribute('dur', duration + "ms")
-    document.getElementById('audioAnimation2').setAttribute('dur', duration + "ms")
-    document.getElementById('audioAnimation1').beginElement()
-    document.getElementById('audioAnimation2').beginElement()
+    if (document.getElementById('audioAnimation1')) {
+        document.getElementById('audioAnimation1').setAttribute('dur', duration + "ms")
+        document.getElementById('audioAnimation2').setAttribute('dur', duration + "ms")
+        document.getElementById('audioAnimation1').beginElement()
+        document.getElementById('audioAnimation2').beginElement()
+    }
 }
 
 export default animateAudio
--- src/utils/resetAudioAnimation.js
+++ src/utils/resetAudioAnimation.js
@@ -1,6 +1,10 @@
 const resetAudioAnimation = () => {
-    document.getElementById('audioAnimation1').endElement()
-    document.getElementById('audioAnimation2').endElement()
+    if (document.getElementById('audioAnimation1')) {
+        document.getElementById('audioAnimation1').endElement()
+    }
+    if (document.getElementById('audioAnimation2')) {
+        document.getElementById('audioAnimation2').endElement()
+    }
 }
 
 export default resetAudioAnimation
