8e9999d176b421fdcae6c5b9040763418ae365af
alexandre-mace <helloalexandremace@protonmail.com>
2022-12-21 03:15:01 +0100

    Update restart component

--- src/components/Restart.js
+++ src/components/Restart.js
@@ -1,12 +1,10 @@
 const Restart = ({handleReset}) => (
     <div className="w-[90%] md:w-full absolute action-btn-bottom left-1/2 -translate-x-1/2 z-50 text-center">
-        <div className={"flex flex-col md:flex-row w-full justify-center"}>
-            <div
-                onClick={() => handleReset()}
-                className="inline-flex whitespace-nowrap justify-center cursor-pointer items-center rounded-full px-6 py-3 text-center btn-primary">
-                Démarrer une nouvelle discussion  💬
-            </div>
-        </div>
+        <button
+            onClick={() => handleReset()}
+            className="inline-block whitespace-nowrap justify-center cursor-pointer items-center rounded-full px-6 py-3 text-center btn-primary">
+            Démarrer une nouvelle discussion  💬
+        </button>
     </div>
 )
 
